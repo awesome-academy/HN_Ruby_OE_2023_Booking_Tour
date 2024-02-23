bf3665f4b85bacc0756fb202fa02eb8dd69ffaca
Vu That <92977052+Rittamx031@users.noreply.github.com>
2024-02-23 11:40:52 +0700

    login with devise

--- db/schema.rb
+++ db/schema.rb
@@ -129,6 +129,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_22_30_709004) do
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
